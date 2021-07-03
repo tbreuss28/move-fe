b71955a4f36f3647fc5b8efdf1ef37bac7a74562
Patrick Schedler <schedler@betterthings.digital>
2021-07-03 17:12:45 +0200

    join move

--- pages/moves/[move].tsx
+++ pages/moves/[move].tsx
@@ -1,13 +1,24 @@
 import { Box, Button, Flex, Heading, Text } from "@chakra-ui/react";
 import { format, parseISO } from "date-fns";
 import { GetServerSideProps } from "next";
+import { useContext } from "react";
 
 import { Footer, Header, SkillLevel } from "@components";
 import { Main } from "@layouts";
 import { api } from "@libs";
+import { Auth } from "@providers";
 import { Move } from "@types";
 
 const MovePage = ({ move }: { move: Move }) => {
+  const { user } = useContext(Auth.Context);
+
+  const handleJoin = async () => {
+    if (!user) {
+      throw new Error("Missing user context!");
+    }
+    await api.post("/moveUsers", { userId: user.id, moveId: move.id });
+  };
+
   return (
     <>
       <Header icon="close" />
@@ -25,6 +36,7 @@ const MovePage = ({ move }: { move: Move }) => {
       </Main>
       <Footer>
         <Button
+          onClick={handleJoin}
           colorScheme="whiteAlpha"
           isFullWidth
           background="brand"
