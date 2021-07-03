703a638d25c1503ba2b41b0b8672590fe527713e
Jean-Pierre Stopinski <j@stopinski.at>
2021-07-03 22:08:39 +0200

    Allow overflow

--- layouts/Main.tsx
+++ layouts/Main.tsx
@@ -2,7 +2,14 @@ import { Flex, FlexProps } from "@chakra-ui/react";
 
 const Main = ({ children }: FlexProps) => {
   return (
-    <Flex as="main" gridArea="main" direction="column" flex="1" p={8}>
+    <Flex
+      as="main"
+      gridArea="main"
+      direction="column"
+      flex="1"
+      p={8}
+      overflowY="scroll"
+    >
       {children}
     </Flex>
   );
