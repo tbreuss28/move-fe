b7d8898f07d21bf6e4a87877bb8b1acb14e80f93
Jean-Pierre Stopinski <j@stopinski.at>
2021-07-03 22:06:26 +0200

    Filter Moves

--- pages/moves/index.tsx
+++ pages/moves/index.tsx
@@ -1,9 +1,9 @@
-import { useContext, useEffect, useState } from "react";
+import { useContext } from "react";
 import { GetServerSideProps } from "next";
 import Router from "next/router";
 import { Box } from "@chakra-ui/react";
-
-import { Header, Footer, Map, Marker } from "@components";
+import { Marker } from "react-google-maps";
+import { Header, Footer, Map } from "@components";
 import { api } from "@libs";
 import { Move } from "@types";
 import { AppNavigation } from "@navigations";
@@ -11,19 +11,6 @@ import { Search } from "@providers";
 
 const MovesPage = ({ moves }: { moves: Move[] }) => {
   const { searchTerm } = useContext(Search.Context);
-  const [filteredMoves, setFilteredMoves] = useState(moves);
-
-  useEffect(() => {
-    if (searchTerm) {
-      const filteredMoves = moves.filter((move) =>
-        move.name.includes(searchTerm)
-      );
-      console.log(filteredMoves);
-    }
-
-    setFilteredMoves(filteredMoves ?? moves);
-  }, [searchTerm, moves]);
-
   return (
     <>
       <Header color="primary" icon="search" />
@@ -33,13 +20,17 @@ const MovesPage = ({ moves }: { moves: Move[] }) => {
           containerElement={<div style={{ height: `100%` }} />}
           mapElement={<div style={{ height: `100%` }} />}
         >
-          {filteredMoves.map((move) => (
-            <Marker
-              key={move.id}
-              position={{ lat: move.latitude, lng: move.longitude }}
-              onClick={() => Router.push(`/moves/${move.id}`)}
-            />
-          ))}
+          {moves
+            .filter((move) =>
+              move.name.toLowerCase().includes(searchTerm?.toLowerCase() ?? "")
+            )
+            .map((move) => (
+              <Marker
+                key={move.id}
+                position={{ lat: move.latitude, lng: move.longitude }}
+                onClick={() => Router.push(`/moves/${move.id}`)}
+              />
+            ))}
         </Map>
       </Box>
       <Footer>
