3df75ad1eeb1d45e375e7c2cf7de4f8eec831843
Jean-Pierre Stopinski <j@stopinski.at>
2021-07-06 20:58:53 +0200

    Add Document Title

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -8,6 +8,7 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
+          <title>Move Now</title>
           <link rel="manifest" href="/manifest.json" />
           <script
             async
